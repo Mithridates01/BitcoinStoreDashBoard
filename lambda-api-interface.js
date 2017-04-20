18ccd37ed49e795026df02a238f2cd42d02d8308
Josh Lucas <jclucas21@gmail.com>
2017-04-20 09:05:26 -0500

    install request module and write request to API

--- lambda-api-interface.js
+++ lambda-api-interface.js
@@ -1,1 +1,8 @@
+var request = require("request");
+
+request("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR", function(error, response, body) {
+  console.log("no error" || error);
+  console.log(body);
+});
+
 
