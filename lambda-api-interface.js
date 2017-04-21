b5ac608f58e94d078abf9fc5d649c28f43f476d9
Mithridates01 <jclucas21@gmail.com>
2017-04-21 10:35:14 -0500

    add code to convert unix time into YYMDD time format

--- lambda-api-interface.js
+++ lambda-api-interface.js
@@ -1,59 +1,56 @@
 var request = require("request");
+var blockchainInfoAPI = "https://api.blockchain.info/charts/market-price?timespan=5weeks&rollingAverage=1days&format=json";
 
-var cryptoCompareAPI = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR";
-var blockchainInfoAPI = "https://api.blockchain.info/charts/Market-Price?timespan=5weeks&format=json";
-
-var testData = [
-    {
-      "x": 1489622400,
-      "y": 1180.945657142857
-    },
-    {
-      "x": 1489708800,
-      "y": 1091.1718875
-    },
-    {
-      "x": 1489795200,
-      "y": 952.2323625
-    },
-    {
-      "x": 1489881600,
-      "y": 1029.8008125000001
-    },
-    {
-      "x": 1489968000,
-      "y": 1049.0844875
-    },
-    {
-      "x": 1490054400,
-      "y": 1118.6300428571428
-    },
-    {
-      "x": 1490140800,
-      "y": 1028.7268625
-    },
-    {
-      "x": 1490227200,
-      "y": 1038.789
-    }
-];
 
 // sen get request to Bitcoin price API
 request( blockchainInfoAPI, function(error, response, body) {
-  if (error) {
-    console.log(error);
+  if (error) {console.log(error);}
+  // Parse to from json string to object
+  var filteredMarketData = ( JSON.parse(body) ).values;
+  // convert unix time to YYMMDD format
+  for (var i = 0; i < filteredMarketData.length; i++) {
+    filteredMarketData[i]["x"] = unixtime2YYMMDD( filteredMarketData[i]["x"] );
   }
 
-                     // JSON.parse(body)
-  var priceData = {data: [ testData ]};
+  console.log(filteredMarketData);
+
+  var priceData = {data: filteredMarketData };
+
+  console.log(priceData)
 
 
   // send data to Cyfe dashboard widget API-endpoint
-  request.post(
-    "https://app.cyfe.com/api/push/58f92260867121476148803224444",
-    { json: priceData },
-    function(error, response, body) {
-      console.log(body);
-    }
-  );
+  // request.post(
+  //   "https://app.cyfe.com/api/push/58f92260867121476148803224444",
+  //   { json: priceData },
+  //   function(error, response, body) {
+  //     console.log(body);
+  //   }
+  // );
 });
+
+
+
+// Time conversion for spreadsheet
+
+
+
+function padZero(number) {
+    if (number < 10) {
+        number = "0" + number;
+    }
+
+    return number;
+}
+
+function unixtime2YYMMDD(unixtime) {
+    var milliseconds = unixtime * 1000,
+        dateObject = new Date(milliseconds),
+        temp = [];
+
+    temp.push(dateObject.getUTCFullYear().toString().slice(2));
+    temp.push(padZero(dateObject.getUTCMonth() + 1));
+    temp.push(padZero(dateObject.getUTCDate()));
+
+    return temp.join("");
+}
