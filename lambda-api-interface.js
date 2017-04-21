2e5a62c501012d9051ea8bcb83941b97a217687e
Mithridates01 <jclucas21@gmail.com>
2017-04-21 11:50:16 -0500

    change dashboard endpoint url

--- lambda-api-interface.js
+++ lambda-api-interface.js
@@ -15,7 +15,7 @@ request( blockchainInfoAPI, function(error, response, body) {
 
   // send data to Cyfe dashboard widget API-endpoint
   request.post(
-    "https://app.cyfe.com/api/push/58fa31fbb5b024784688903226664",
+    "https://app.cyfe.com/api/push/58fa37bbdb79f0278724893226715",
     { json: priceData },
     function(error, response, body) {
       console.log(body);
