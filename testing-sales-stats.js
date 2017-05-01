fc98e337b79341bd76c15b46499d5bb4b1a67ea7
Josh Lucas <jclucas21@gmail.com>
2017-05-01 18:18:14 -0500

    add report json, post request, working API hits

--- testing-sales-stats.js
+++ testing-sales-stats.js
@@ -1,15 +1,39 @@
-// var request = require("request");
+var request = require("request");
 
 
 
 var apiKey      = process.env.SPOTIFY_API_KEY;
 var apiPassword = process.env.SPOTIFY_API_PASSWORD; //my be same as key
-var hostname    = "https://bitcoin-com-store.myshopify.com";
-var path        = "";
+var hostname    = "bitcoin-com-store.myshopify.com";
+var path        = "/admin/reports.json";
 var query       = "";
 
+// ORDER DATES AND PRICES
+// https://bitcoin-com-store.myshopify.com/admin/orders.json?status=any&created_at_min=2017-04-01&created_at_max=2017-05-01&fields=created-at,total-price&limit=250&page=1
+// COUNT of orders
+// https://bitcoin-com-store.myshopify.com/admin/orders/count.json?status=any&created_at_min=2017-04-01&created_at_max=2017-05-01
 
-var spotifySalesApi = apiKey + ":" + apiPassword + "@" + path + query;
+var createReport = {
+  "report": {
+    "name": "A new app report",
+    "shopify_ql": "SHOW total_sales BY country FROM order ORDER BY total_sales SINCE -1m UNTIL today"
+  }
+}
+
+var spotifySalesApi = "https://" + apiKey + ":" + apiPassword + "@" + hostname + path ;
+
+
+
+request.post(
+  spotifySalesApi,
+  {json: createReport},
+  function(error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(body);
+  }
+});
 
 request( spotifySalesApi, function(error, response, body) {
   if (error) {
@@ -18,4 +42,3 @@ request( spotifySalesApi, function(error, response, body) {
     console.log(body);
   }
 });
-
