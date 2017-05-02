48507c946228f1e9145429e8d910f45f1c478a16
Josh Lucas <jclucas21@gmail.com>
2017-05-02 10:09:59 -0500

    add comment sections, start new implementation

--- testing-sales-stats.js
+++ testing-sales-stats.js
@@ -1,33 +1,26 @@
 var request = require("request");
-
-
-
-var apiKey      = process.env.SPOTIFY_API_KEY;
-var apiPassword = process.env.SPOTIFY_API_PASSWORD; //my be same as key
-var hostname    = "bitcoin-com-store.myshopify.com";
-var path        = "/admin/reports.json";
-var query       = "";
-
-// ORDER DATES AND PRICES
 // https://bitcoin-com-store.myshopify.com/admin/orders.json?status=any&created_at_min=2017-04-01&created_at_max=2017-05-01&fields=created-at,total-price&limit=250&page=1
 // COUNT of orders
 // https://bitcoin-com-store.myshopify.com/admin/orders/count.json?status=any&created_at_min=2017-04-01&created_at_max=2017-05-01
 
-var createReport = {
-  "report": {
-    "name": "A new app report",
-    "shopify_ql": "SHOW total_sales BY country FROM order ORDER BY total_sales SINCE -1m UNTIL today"
-  }
-}
+// SPLIT IMPLEMENTATION
+// DUE to Shopify API request records limitation of 250 max; This will check total number of purchases for last 30 days
+// and then split into multiple requests if total is greater than 250; then reassemble into one object
+var apiKey             = process.env.SPOTIFY_API_KEY;
+var apiPassword        = process.env.SPOTIFY_API_PASSWORD; //my be same as key
+var hostname           = "bitcoin-com-store.myshopify.com";
+var orderRecordsPath   = "/admin/reports.json";
+var orderCountPath     = "/admin/orders/count.json";
+var orderRecordsParams = "?status=any&created_at_min=2017-04-01&created_at_max=2017-05-01&fields=created-at,total-price&limit=250&page=1";
+var orderCountParams   = "?status=any&created_at_min=" + "2017-04-01" + "&created_at_max=" + "2017-05-01";
 
-var spotifySalesApi = "https://" + apiKey + ":" + apiPassword + "@" + hostname + path ;
+// last 30 days
+var shopifyOrderRecords = "https://" + apiKey + ":" + apiPassword + "@" + hostname ;
+var shopifyOrderCount = "https://" + apiKey + ":" + apiPassword + "@" + hostname + orderCountPath + orderCountParams;
 
-
-
-request.post(
-  spotifySalesApi,
-  {json: createReport},
-  function(error, response, body) {
+// check order count
+var orderCount;
+request( shopifyOrderCount, function(error, response, body) {
   if (error) {
     console.log(error);
   } else {
@@ -35,10 +28,60 @@ request.post(
   }
 });
 
-request( spotifySalesApi, function(error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(body);
-  }
-});
+
+// request( spotifySalesApi, function(error, response, body) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(body);
+//   }
+// });
+
+
+
+
+
+
+
+
+// CREATE REPORT IMPLEMENTATION
+// THIS CODE SHOULD CREATE A SALES REPORT WITH POST AND THEN BE ABLE TO PULL REPORT WITH REQUEST.
+// WHEN SITE HAS MORE TRAFFIC THIS SHOULD BE IMPLEMENTED TO IMPROVE EFFICIENCY.
+
+// var apiKey      = process.env.SPOTIFY_API_KEY;
+// var apiPassword = process.env.SPOTIFY_API_PASSWORD; //my be same as key
+// var hostname    = "bitcoin-com-store.myshopify.com";
+// var path        = "/admin/reports.json";
+// var query       = "";
+
+// ORDER DATES AND PRICES
+
+// var createReport = {
+//   "report": {
+//     "name": "A new app report",
+//     "shopify_ql": "SHOW total_sales BY country FROM order ORDER BY total_sales SINCE -1m UNTIL today"
+//   }
+// }
+
+// var spotifySalesApi = "https://" + apiKey + ":" + apiPassword + "@" + hostname + path ;
+
+
+
+// request.post(
+//   spotifySalesApi,
+//   {json: createReport},
+//   function(error, response, body) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(body);
+//   }
+// });
+
+// request( spotifySalesApi, function(error, response, body) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(body);
+//   }
+// });
